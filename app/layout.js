efa4cbf6e0c388e968f1dd54255e0d3422836fb2
andreiana007 <163748348+andreiana007@users.noreply.github.com>
2026-03-30 21:20:56 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 export default function RootLayout({ children }) {
   return (
     <html lang="ro">
+    <link
+  href="https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;700&display=swap"
+  rel="stylesheet"
+/>
       <body
         style={{
           margin: 0,
