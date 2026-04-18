3990fa2a61f4860f52e9ecbc0bf62e20e2ef1065
andreiana007 <163748348+andreiana007@users.noreply.github.com>
2026-04-18 14:49:00 +0300

    Update page.js

--- app/pisica/page.js
+++ app/pisica/page.js
@@ -166,6 +166,7 @@ export default function PisicaPage() {
               borderRadius: "10px",
               border: "1px solid #111",
               fontFamily: '"Courier Prime", "Courier New", monospace',
+              fontSize "16px",
             }}
           />
 
@@ -180,6 +181,7 @@ export default function PisicaPage() {
               cursor: "pointer",
               fontWeight: "bold",
               fontFamily: '"Courier Prime", "Courier New", monospace',
+              fontSize "16px",
             }}
           >
             Miau
